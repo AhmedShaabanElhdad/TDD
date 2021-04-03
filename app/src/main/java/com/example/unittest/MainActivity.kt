ea89c156ca3c3ff8fbbcd5b5505f3d02aeab0cb7
AhmedShaabanElhdad <ahmed.shaaban.elhdah@gmail.com>
2021-04-03 16:34:57 +0200

    initialize the app with all dependency we will use

---
+++ app/src/main/java/com/example/unittest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.unittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.text.isDigitsOnly
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
